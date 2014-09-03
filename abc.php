216ee81c9bfb6424ed45fc537c5746a7e45e8e34
Abhilash <abhilash.ma@live.in>
2014-09-03 15:14:12 +0530

    added abc.php file in root on 03-08-2014 3.14

--- abc.php
+++ abc.php
@@ -5,6 +5,16 @@
 	<title>ABC.php</title>
 </head>
 <body>
-	
+	<div>
+		<header>
+			<ul>
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+			</ul>
+		</header>
+		<footer>
+			<p></p>
+		</footer>
+	</div>
 </body>
 </html>
