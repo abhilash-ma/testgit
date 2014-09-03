67268d198086bd5bd256602b144e13817b760f4c
Abhilash <abhilash.ma@live.in>
2014-09-03 16:08:53 +0530

    Added new file index.php on 03-08-2014

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test Page</title>
+</head>
+<body>
+	<div>
+		<header>
+			<ul>
+				<li><a href="#">Link 1</a></li>
+				<li><a href="#">Link 2</a></li>
+			</ul>
+		</header>
+		<footer>
+			<p>Footer</p>
+		</footer>
+	</div>
+</body>
+</html>
